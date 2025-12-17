bd89a2d757450f0e8dba9d5068e5144b0708c5b7
hari <hariprasad8976@gmail.com>
2025-12-17 07:18:30 +0000

    login form design updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,37 +1,48 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { MaterialModule } from '../shared/material-module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterLink, MaterialModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, MaterialModule,],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  protected readonly submitted = signal(false);
-
-  protected form!: any;
+  // protected readonly submitted = signal(false);
+  // protected form!: any;
+  // constructor(private fb: FormBuilder) {
+  //   this.form = this.fb.group({
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', [Validators.required, Validators.minLength(6)]]
+  //   });
+  // }
+  // protected onSubmit(): void {
+  //   this.submitted.set(true);
+  //   if (this.form.valid) {
+  //     // eslint-disable-next-line no-console
+  //     console.log('Login attempt:', this.form.value);
+  //   }
+  // }
+  // protected control(name: string) {
+  //   return this.form.get(name);
+  // }
+  // -----------------------------------------------------------------------------
+  loginForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
+    this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
-  protected onSubmit(): void {
-    this.submitted.set(true);
-    if (this.form.valid) {
-      // eslint-disable-next-line no-console
-      console.log('Login attempt:', this.form.value);
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
     }
-  }
-
-  protected control(name: string) {
-    return this.form.get(name);
   }
 }
