1cce22899adc2b8e22451b02fb22cd4c0096a500
Rageeniverma <114106713+Rageeniverma@users.noreply.github.com>
2026-01-10 10:27:52 +0000

    port 8003

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://obscure-space-garbanzo-94xr7ww9j4vcppj-8000.app.github.dev/api'
+  apiUrl: 'https://obscure-space-garbanzo-94xr7ww9j4vcppj-8003.app.github.dev/api'
 };
